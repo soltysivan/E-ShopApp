84ba343b828356c9515b731c2a8530c091b70381
soltisivan <soltisivan11@gmail.com>
2019-08-01 16:38:23 +0300

    fix bug

--- src/main/java/org/shop/service/CommentService.java
+++ src/main/java/org/shop/service/CommentService.java
@@ -15,48 +15,48 @@ import java.util.List;
 @Service
 public class CommentService {
 
-//    @Autowired
-//    private CommentRepository commentRepository;
-//
-//    @Autowired
-//    private ProductRepository productRepository;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//
-//    public List<Comment> findAllCommentsByProduct(Long productId) {
-//        Product product = productRepository.findById(productId)
-//                .orElseThrow(NotFoundExceptions::new);
-//        List<Comment> comments = product.getComments();
-//        return comments;
-//    }
-//
-//    public Comment saveComment(Long productId, Long authorId, Comment comment) {
-//        User author = userRepository.findById(authorId)
-//                .orElseThrow(NotFoundExceptions::new);
-//        Product product = productRepository.findById(productId)
-//                .orElseThrow(NotFoundExceptions::new);
-//        comment.setUser(author);
-//        comment.setProduct(product);
-//        commentRepository.save(comment);
-//        return comment;
-//    }
-//
-//    public Comment updateComment(Long id, Comment comment) {
-//        Comment commentFromDB = commentRepository.findById(id)
-//                .orElseThrow(NotFoundExceptions::new);
-//        commentFromDB.setText(comment.getText());
-//        return commentRepository.save(commentFromDB);
-//    }
-//
-//    public Comment saveCommentsToComment(Long commentId, Comment comment) {
-//        Comment commentFromDb = commentRepository.findById(commentId)
-//                .orElseThrow(NotFoundExceptions::new);
-//        comment.setUser(commentFromDb.getUser());
-//        comment.setProduct(null);
-//        comment.setComment(commentFromDb.getId());
-//        commentRepository.save(comment);
-//        return comment;
-//    }
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<Comment> findAllCommentsByProduct(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(NotFoundExceptions::new);
+        List<Comment> comments = product.getComments();
+        return comments;
+    }
+
+    public Comment saveComment(Long productId, Long authorId, Comment comment) {
+        User author = userRepository.findById(authorId)
+                .orElseThrow(NotFoundExceptions::new);
+        Product product = productRepository.findById(productId)
+                .orElseThrow(NotFoundExceptions::new);
+        comment.setUser(author);
+        comment.setProduct(product);
+        commentRepository.save(comment);
+        return comment;
+    }
+
+    public Comment updateComment(Long id, Comment comment) {
+        Comment commentFromDB = commentRepository.findById(id)
+                .orElseThrow(NotFoundExceptions::new);
+        commentFromDB.setText(comment.getText());
+        return commentRepository.save(commentFromDB);
+    }
+
+    public Comment saveCommentsToComment(Long commentId, Comment comment) {
+        Comment commentFromDb = commentRepository.findById(commentId)
+                .orElseThrow(NotFoundExceptions::new);
+        comment.setUser(commentFromDb.getUser());
+        comment.setProduct(null);
+        comment.setComment(commentFromDb.getId());
+        commentRepository.save(comment);
+        return comment;
+    }
 }
